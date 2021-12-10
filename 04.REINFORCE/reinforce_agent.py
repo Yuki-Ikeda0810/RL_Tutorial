18b6c852c58659405b7d708f080cb13d63337338
Yuki-Ikeda0810 <yukiikeda2@gmail.com>
2021-12-10 19:18:23 +0900

    add reinforce_tutorial.py

---
+++ 04.REINFORCE/reinforce_agent.py
@@ -0,0 +1,75 @@
+# coding:utf-8
+
+
+""" 方策に基づく手法 (Policy-based Methods)
+    今回は，方策に基づく代表的な手法として，REINFORCEを扱います．
+"""
+
+# 必要なライブラリのインポート．
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical, Normal
+
+""" REINFORCE
+    今回は，方策を直接ニューラルネットワークで関数近似します．
+    (実際には，方策と価値関数の両方を関数近似するactor-criticの手法を用いることも多いです．)
+    REINFORCEでは，確率的な方策を採用しています．
+"""
+
+# 方策のネットワークの定義
+class PolicyNetwork(nn.Module):
+    def __init__(self, num_state, num_action, hidden_size=16):
+        super(PolicyNetwork, self).__init__()
+        self.fc1 = nn.Linear(num_state, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_action)
+    
+    def forward(self, x):
+        h = F.elu(self.fc1(x))
+        h = F.elu(self.fc2(h))
+        action_prob = F.softmax(self.fc3(h), dim=-1)
+        return action_prob
+
+# REINFORCEエージェントのクラス．
+class ReinforceAgent:
+    def __init__(self, num_state, num_action, gamma=0.99, lr=0.001):
+        self.num_state = num_state
+        self.gamma = gamma  # 割引率
+        self.pinet = PolicyNetwork(num_state, num_action)
+        self.optimizer = optim.Adam(self.pinet.parameters(), lr=lr)
+        self.memory = []  # 報酬とそのときの行動選択確率のtupleをlistで保存
+    
+    # 方策を更新
+    def update_policy(self):
+        R = 0
+        loss = 0
+        # エピソード内の各ステップの収益を後ろから計算
+        for r, prob in self.memory[::-1]:
+            R = r + self.gamma * R
+            loss -= torch.log(prob) * R
+        loss = loss/len(self.memory)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+    
+    # softmaxの出力が最も大きい行動を選択
+    def get_greedy_action(self, state):
+        state_tensor = torch.tensor(state, dtype=torch.float).view(-1, self.num_state)
+        action_prob = self.pinet(state_tensor.data).squeeze()
+        action = torch.argmax(action_prob.data).item()
+        return action
+    
+    # カテゴリカル分布からサンプリングして行動を選択
+    def get_action(self, state):
+        state_tensor = torch.tensor(state, dtype=torch.float).view(-1, self.num_state)
+        action_prob = self.pinet(state_tensor.data).squeeze()
+        action = Categorical(action_prob).sample().item()
+        return action, action_prob[action]
+    
+    def add_memory(self, r, prob):
+        self.memory.append((r, prob))
+    
+    def reset_memory(self):
+        self.memory = []
---
+++ 04.REINFORCE/reinforce_tutorial.py
@@ -0,0 +1,93 @@
+# coding:utf-8
+
+
+""" 方策に基づく手法 (Policy-based Methods)
+    今回は，方策に基づく代表的な手法として，REINFORCEを扱います．
+"""
+
+# 必要なライブラリのインポート．
+import numpy as np
+import gym
+from gym import wrappers
+import matplotlib.pyplot as plt
+
+# 「reinforce_agent_agent.py」をインポート．
+import reinforce_agent
+
+
+""" 各種設定
+    学習に必要なパラメータの設定をします．
+"""
+
+# 学習の設定．
+num_episode         = 600   # 学習エピソード数．
+# penalty           = 10    # 途中でエピソードが終了したときのペナルティ
+
+# ログ用の設定．
+episode_rewards = []
+num_average_epidodes = 10
+
+env = gym.make('CartPole-v0')          # シミュレータ環境の構築．
+max_steps = env.spec.max_episode_steps # エピソードの最大ステップ数．
+
+# REINFORCEエージェントのインスタンスを作成．
+agent = reinforce_agent.ReinforceAgent(env.observation_space.shape[0], env.action_space.n)
+
+
+
+""" REINFORCEエージェントの学習
+    今回は，エージェントの学習に，CartPole-v0と呼ばれる，台車に振子がついた環境を利用します．
+    参考：https://github.com/openai/gym/wiki/CartPole-v0
+"""
+
+for episode in range(num_episode):
+    state = env.reset()  # envからは4次元の連続値の観測が返ってくる．
+    episode_reward = 0
+    for t in range(max_steps):
+        action, prob = agent.get_action(state)  #  行動を選択．
+        next_state, reward, done, _ = env.step(action)
+#         # もしエピソードの途中で終了してしまったらペナルティを加える．
+#         if done and t < max_steps - 1:
+#             reward = - penalty
+        episode_reward += reward
+        agent.add_memory(reward, prob)
+        state = next_state
+        if done:
+            agent.update_policy()
+            agent.reset_memory()
+            break
+    episode_rewards.append(episode_reward)
+    if episode % 20 == 0:
+        print("Episode %3d finished | Episode reward %8f" % (episode, episode_reward))
+
+# 累積報酬の移動平均を表示．
+moving_average = np.convolve(episode_rewards, np.ones(num_average_epidodes)/num_average_epidodes, mode='valid')
+plt.plot(np.arange(len(moving_average)),moving_average)
+plt.title('REINFORCE: average rewards in %d episodes' % num_average_epidodes)
+plt.xlabel('episode')
+plt.ylabel('rewards')
+plt.show()
+
+env.close()
+
+
+""" 最終的に得られた方策のテスト(可視化)
+    5回のエピソードをシミュレーターで動作させ，学習後のエージェントの方策を可視化します．
+"""
+
+# シミュレータ環境の構築．
+env = gym.make('CartPole-v0')
+env = wrappers.Monitor(env, "./movie", force=True)
+
+# 5回のエピソードをシミュレーターで動作させる．
+for i_episode in range(5):
+    observation = env.reset()                             # エピソードを開始(環境の初期化)．
+    env.render()                                          # シミュレータ画面の出力．
+
+    done = False
+    while not done:
+        action = agent.get_greedy_action(state)                    # エージェントによる行動を取得．
+        next_observation, reward, done, _ = env.step(action)       # 行動を実行し，次の状態，報酬，終端か否かの情報を取得．
+    
+# 画面出力の終了．
+env.close()
